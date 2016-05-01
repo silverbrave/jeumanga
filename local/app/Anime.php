b6eef8d4de0e6b0f89adfb42f6bdb4cdb19a60e9
florian <florian69780@hotmail.fr>
2016-05-01 14:23:42 +0200

    dzef
    Signed-off-by: florian <florian69780@hotmail.fr>

---
+++ local/app/Anime.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Anime extends Model
+{
+    protected $fillable=['id','nom','nb_ep','synopsis','imgAnime','logo','op','idgenre'];
+}
